9883000b293989ed95e153a2d3d86675af108c02
Shardic <marco.geils@freenet.de>
2015-06-08 10:20:26 +0200

    hallo welt hinzugefuegt

--- IntelliJ/src/main.java
+++ IntelliJ/src/main.java
@@ -6,14 +6,7 @@ public class main {
     //von Fabian
     //von oli
     //Konrad
-<<<<<<< HEAD
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hallo Welt!");
-=======
-
-    public static void main(String [] args)
-    {
-        System.out.println("Moin Moin");
->>>>>>> 8e71919f1a810b04d7d42162b38f3ec58800f574
     }
 }
