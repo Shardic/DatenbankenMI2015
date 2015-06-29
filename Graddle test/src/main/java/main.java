8ca527496731a2fe4678e035947015f00721d22c
bigf327 <Fabian@dhcp-vl203-9-46.hs-bremen.de>
2015-06-29 08:03:20 +0200

    Gradle aufgesetzt

---
+++ Graddle test/src/main/java/main.java
@@ -0,0 +1,20 @@
+import org.hibernate.HibernateException;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+import javax.security.auth.login.Configuration;
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * Created by Fabian on 27.06.15.
+ */
+public class main {
+    public static void main(String [] args) {
+        System.out.println("Hello World");
+        TestClass h = new TestClass();
+        h.setUsername("hahha");
+        System.out.println(h.getUsername());
+        DBConnection d = new DBConnection();
+
+    }
+}
