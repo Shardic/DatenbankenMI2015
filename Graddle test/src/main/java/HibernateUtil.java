a47ace7032139f8ba27a3365ca95ade337b42ccf
bigf327 <Fabian@dhcp-vl203-0-84.hs-bremen.de>
2015-07-11 11:46:06 +0200

    Gadle.gradle & hibernate.cfg bearbeitet HibernateUtil.java erstellt

---
+++ Graddle test/src/main/java/HibernateUtil.java
@@ -0,0 +1,36 @@
+/**
+ * Created by Fabian on 11.07.15.
+ */
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+    private static SessionFactory mSessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory(){
+        try{
+            if(mSessionFactory == null){
+                Configuration configuration = new Configuration().configure(HibernateUtil.class.getResource("/hibernate.cfg.xml"));
+                StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
+                serviceRegistryBuilder.applySettings(configuration.getProperties());
+                ServiceRegistry serviceRegistry = serviceRegistryBuilder.build();
+                mSessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            }
+            return mSessionFactory;
+        } catch(Throwable e){
+            System.err.println("Initial SessionFactory creation failed." + e);
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory(){
+        return mSessionFactory;
+    }
+
+    public static void shutdown()
+    {
+        getSessionFactory().close();
+    }
+}
--- Graddle test/src/main/java/main.java
+++ Graddle test/src/main/java/main.java
@@ -1,8 +1,11 @@
-import org.hibernate.HibernateException;
-
+import Tabels.Kunde;
 import javax.security.auth.login.Configuration;
-import java.io.IOException;
-import java.sql.SQLException;
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+import org.hibernate.*;
+
 
 /**
  * Created by Fabian on 27.06.15.
@@ -10,9 +13,28 @@ import java.sql.SQLException;
 public class main {
     public static void main(String [] args) {
         System.out.println("Hello World");
-        TestClass h = new TestClass();
-        h.setUsername("hahha");
-        System.out.println(h.getUsername());
+
+
         //DBConnection d = new DBConnection();
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            Criteria criteria = session.createCriteria(Kunde.class);
+            List allUsers = criteria.list();
+            System.out.print("HIER" + allUsers.get(0));
+
+            transaction.commit();
+        }
+        catch (Exception e) {
+            System.err.print("HIER FEHLER");
+            if (transaction!=null) transaction.rollback();
+            throw e;
+        }
+        finally {
+            session.close();
+        }
+
     }
 }
